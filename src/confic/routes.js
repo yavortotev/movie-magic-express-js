03093e05eed2178ead401e14823d99da9bf76731
yavortotev <yavortotev@yahoo.com>
2024-09-30 21:42:29 +0300

    add cast

--- src/confic/routes.js
+++ src/confic/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const { home, details, search }  = require('../controllers/catalog')
 const { about } = require('../controllers/about')
 const { createGet, createPost } = require('../controllers/movie')
+const { createGet : createCastGet, createPost : createCastPost } = require('../controllers/cast')
 const { notFound } = require('../controllers/404')
 
 
@@ -10,9 +11,13 @@ const router = Router()
 router.get('/',home)
 router.get('/details/:id',details)
 router.get('/about',about)
-router.get('/create', createGet)
-router.post('/create', createPost)
+router.get('/create/movie', createGet)
+router.post('/create/movie', createPost)
+router.get('/create/cast', createCastGet)
+router.post('/create/cast', createCastPost)
 router.get('/search', search)
+
+//TODO post create case 
 
 
 router.get('*', notFound)
---
+++ src/controllers/cast.js
@@ -0,0 +1,15 @@
+module.exports = {
+  
+    createGet: (req, res)=> {
+        res.render('cast-create')
+    },
+
+    createPost : (req , res) => {
+        console.log(req.body);
+
+        res.end()
+        
+
+    }
+     
+}
